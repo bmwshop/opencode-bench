86b8d48d0ba53ad70867eafd702d73609f7c3e22
Dima Rekesh <bmwshop@gmail.com>
2026-05-03 09:04:00 -0700

    eval script

--- cluster_scripts/cw-dfw-direct/nano_v3_sft.sh
+++ cluster_scripts/cw-dfw-direct/nano_v3_sft.sh
@@ -4,13 +4,16 @@ REPO_ROOT=$(cd "${SCRIPT_DIR}/../.." && pwd)
 CONFIG_DIR=${REPO_ROOT}/cluster_configs
 
 MODEL="sft-all_0417.final.alex"
-STEPS="100 200 300"
+MODEL="sft-all_0502.final_alex"
+MODEL="sft-all_0502b.final_alex"
+STEPS="100 200 300 400 500 600"
 BASE_DIR=/lustre/fsw/portfolios/llmservice/users/drekesh/results/sft/${MODEL}
 MODEL_MOUNT=${BASE_DIR}:${BASE_DIR}
 TP=8
 CLUSTER=cw-dfw-direct
 PARTITION=batch_short
 JOBS=4
+NEVALS=4
 TIMEOUT=450
 RETRIES=5
 OUTPUT_DIR=/lustre/fsw/portfolios/llmservice/users/$USER/opencode-bench-results
@@ -30,6 +33,7 @@ python "${REPO_ROOT}/run_cluster.py"  \
 --server-gpus 8 \
 --output-dir ${OUTPUT_DIR} \
 --expname ${MODEL}-${ITER} \
+--parallel-jobs=${NEVALS} \
 --partition ${PARTITION} \
 --skip-schema-check \
 --server-args "--tensor-parallel-size ${TP} --gpu-memory-utilization 0.8 --enable-auto-tool-choice --tool-call-parser qwen3_coder --reasoning-parser deepseek_r1 --mamba_ssm_cache_dtype float32"
